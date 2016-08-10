b5dec479745794e4b3949cac7fb2ca18ee477fe5
nejni-marji <nejni.marji@gmail.com>
2016-08-10 16:35:14 -0500

    Translated Base.start()

--- Base.py
+++ Base.py
@@ -3,7 +3,7 @@ import telegram as tg
 import telegram.ext as tg_ext
 
 def start(bot, update):
-	bot.sendMessage(update.message.chat_id, 'Started!')
+	bot.sendMessage(update.message.chat_id, 'Komencita!')
 
 def main(dp):
 	dp.add_handler(tg_ext.CommandHandler('start', start))
