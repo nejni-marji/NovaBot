c702edc827812c60d8a550dcce78011daf95a4b0
nejni-marji <nejni.marji@gmail.com>
2016-08-10 12:11:30 -0500

    Changed TextParse.bang() so it only runs bot.sendMessage() if there is a match

--- TextParse.py
+++ TextParse.py
@@ -7,10 +7,10 @@ def text_parse(bot, update):
 	bang(bot, update)
 
 def bang(bot, update):
-	text = update.message.text
-	bang = re.match('![a-zA-Z0-9]*', text).group()[1:]
-	resp = 'You used the bang: `{}`'.format(bang)
-	bot.sendMessage(update.message.chat_id, resp)
+	bang = re.match('![a-zA-Z0-9]*', update.message.text)
+	if bang:
+		resp = 'You used the bang: `{}`'.format(bang.group()[1:])
+		bot.sendMessage(update.message.chat_id, resp)
 
 def main(dp):
 	dp.add_handler(tg_ext.MessageHandler([tg_ext.Filters.text], text_parse))
