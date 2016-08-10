d2039bc2464c5427f3cf2b13679e06b3ce3e674e
nejni-marji <nejni.marji@gmail.com>
2016-08-10 12:08:17 -0500

    Modularized TextParse

--- TextParse.py
+++ TextParse.py
@@ -3,6 +3,9 @@ import re
 import telegram as tg
 import telegram.ext as tg_ext
 
+def text_parse(bot, update):
+	bang(bot, update)
+
 def bang(bot, update):
 	text = update.message.text
 	bang = re.match('![a-zA-Z0-9]*', text).group()[1:]
@@ -10,4 +13,4 @@ def bang(bot, update):
 	bot.sendMessage(update.message.chat_id, resp)
 
 def main(dp):
-	dp.add_handler(tg_ext.MessageHandler([tg_ext.Filters.text], bang))
+	dp.add_handler(tg_ext.MessageHandler([tg_ext.Filters.text], text_parse))
