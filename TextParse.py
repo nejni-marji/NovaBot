4b80c8b323559ad7fdfb3fc5364652b630c14613
nejni-marji <nejni.marji@gmail.com>
2016-08-10 16:17:53 -0500

    Changed TextParse.bang() to not display used bang in Telegram

--- TextParse.py
+++ TextParse.py
@@ -10,8 +10,9 @@ def bang(bot, update):
 	bang = re.match('![a-zA-Z0-9]*', update.message.text)
 	if bang:
 		resp = 'You used the bang: *{}*'.format(bang.group())
-		bot.sendMessage(update.message.chat_id, resp,
-		                parse_mode = tg.ParseMode.MARKDOWN)
+		print(resp)
+		#bot.sendMessage(update.message.chat_id, resp,
+		                #parse_mode = tg.ParseMode.MARKDOWN)
 
 def main(dp):
 	dp.add_handler(tg_ext.MessageHandler([tg_ext.Filters.text], text_parse))
