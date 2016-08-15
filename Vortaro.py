037ae2e87d4ebf30c07c95324a597ea43a07d100
nejni-marji <nejni.marji@gmail.com>
2016-08-15 13:51:47 -0500

    Changed Vortaro.lookup()

--- Vortaro.py
+++ Vortaro.py
@@ -3,12 +3,17 @@ import re
 import telegram as tg
 import telegram.ext as tg_ext
 
-espdic = open('private/espdic.txt').read().split('\n')
-espdic.pop(0)
-espdic.remove('')
+espdic_lines = open('private/espdic.txt').read().split('\n')
+espdic_lines.pop(0)
+espdic_lines.remove('')
+espdic = {}
+for line in espdic_lines:
+	word = line.split(' : ')[0]
+	definition = line.split(' : ')[1].split(', ')
+	espdic[word] = definition
 
 def vortaro(bot, update, args):
-	bot.sendMessage(update.message.from_user.id, lookup(args),
+	bot.sendMessage(update.message.from_user.id, lookup(' '.join(args)),
 	                parse_mode = tg.ParseMode.MARKDOWN)
 	resp = 'Se vi ne ricevis mian privatan mesaĝon, bonvolu private mesaĝi min je /start.'
 	bot.sendMessage(update.message.chat_id, resp,
@@ -18,13 +23,12 @@ def vortaro(bot, update, args):
 def lookup(args):
 	if not args:
 		return 'Vi devas specifi vorton aŭ vortojn.'
-	regex = re.compile(r'\b{}\b'.format(' '.join(args)), re.I)
 	results = []
 	for line in espdic:
-		search = regex.search(line)
-		if search:
-			search = re.sub(r'(.*) : (.*)', r'*\1*: _\2_', search.string)
-			results.append(search)
+		check = line.lower(), *list(i.lower() for i in espdic[line])
+		if args in check or 'to ' + args in check:
+			results.append('*{}*: _{}_'.format(line, ', '.join(espdic[line])))
+	return '\n'.join(sorted(results))
 	if not results:
 		return 'Mi trovis nenion.'
 	return '\n'.join(results)
