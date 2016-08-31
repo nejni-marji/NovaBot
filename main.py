b7dd367d701aea9c019fe7092c70c2903bf7bbf0
nejni-marji <nejni.marji@gmail.com>
2016-08-31 12:18:09 -0500

    Changed package names to lowercase

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import telegram as tg
 import telegram.ext as tg_ext
 
 def load():
-	import Base
-	Base.main(dp)
-	import TextParse
-	TextParse.main(dp)
-	import Utils
-	Utils.main(dp)
-	import Vortaro
-	Vortaro.main(dp)
+	import base
+	base.main(dp)
+	import text_parse
+	text_parse.main(dp)
+	import utils
+	utils.main(dp)
+	import vortaro
+	vortaro.main(dp)
 
 updater = tg_ext.Updater((open('private/token.txt').read()))
 dp = updater.dispatcher
